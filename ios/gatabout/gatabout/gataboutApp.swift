02df8c23e75fcd2dfb59cc62e44b46f3c18dda85
Sam Nichols <shizam@gmail.com>
2026-04-11 19:59:57 -0700

    first

---
+++ ios/gatabout/gatabout/gataboutApp.swift
@@ -0,0 +1,17 @@
+//
+//  gataboutApp.swift
+//  gatabout
+//
+//  Created by Sam Nichols on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct gataboutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
